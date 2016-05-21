fe4350810bba46028cab77ff92167561b678f806
Phil Gibbins <phil@philgibbins.com>
2016-05-21 17:57:44 +0100

    Added basic CSS, started JS and got bg img

---
+++ js/components/menu.js
@@ -0,0 +1,16 @@
+;var band = {};
+band.menu = (function() {
+            
+    // private methods
+    var init = function() {
+        // menu click funxtionality
+    };
+
+    // public methods
+    return {
+        init: init
+    }
+
+})();
+band.menu.init();
+delete band.menu.init;
