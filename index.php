ec3cd3716e0cc2866c439622551592b14e523e37
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-06-02 21:40:50 +0100

    Initial commit of working Periscope broadcast

--- index.php
+++ index.php
@@ -15,6 +15,14 @@
             <h1>Band Name</h1>
             <p>Upcoming legends in indie rock</p>
         </main>
+        <aside>
+            <h2>Watch the band live!</h2>
+            <p>Click the following link to watch the band play live when they are broadcasting an event. When nothing is being broadcasted, you can check out the previous broadcasts that are saved on their profile page by clicking the same link.</p>
+            <script>window.twttr=function(t,e,r){var n,i=t.getElementsByTagName(e)[0],w=window.twttr||{};return t.getElementById(r)?w:(n=t.createElement(e),n.id=r,n.src="https://platform.twitter.com/widgets.js",i.parentNode.insertBefore(n,i),w._e=[],w.ready=function(t){w._e.push(t)},w)}(document,"script","twitter-wjs")</script>
+            <a href="https://www.periscope.tv/scoobster17" class="periscope-on-air" data-size="large">
+                @Scoobster17
+            </a>
+        </aside>
         <!--<aside>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/F9CRooinD5U" frameborder="0" allowfullscreen></iframe>
         </aside>-->
